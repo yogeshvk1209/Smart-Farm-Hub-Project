4c548a7611596f654264414f62d394a0f245f1ef
yogeshvk1209 <yogeshvk1209@gmail.com>
2026-01-02 22:07:59 +0530

    Updating some docs

--- backend/function_ingest-farm-data/main.py
+++ backend/function_ingest-farm-data/main.py
@@ -2,8 +2,13 @@ import functions_framework
 from google.cloud import bigquery
 import datetime
 
+# --- CONFIG ---
+# CHANGE THIS to something complex!
+#  update your Secrete here
+#API_SECRET = "FARM_SECRET"
+
 # CONFIG (Update if needed)
-PROJECT_ID = "farm-hub"
+PROJECT_ID = "farm-hub-482111"
 DATASET_ID = "farm_telemetry"
 TABLE_ID = "soil_readings"
 
@@ -12,29 +17,25 @@ table_ref = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
 
 @functions_framework.http
 def ingest_data(request):
-    """
-    Ingest IoT data via GET (Query Params) or POST (JSON).
-    """
-    # 1. Try to get JSON (for testing)
-    request_data = request.get_json(silent=True)
+    # 1. SECURITY CHECK
+    # We look for a query param '?token=...'
+    request_args = request.args
+    token = request_args.get('token')
     
-    # 2. If no JSON, grab URL Query Params (For the Modem)
-    if not request_data:
-        request_data = request.args
+    if token != API_SECRET:
+        print(f"Unauthorized access attempt. Token: {token}")
+        return 'Unauthorized', 401
 
-    if not request_data:
-        return 'Missing Payload', 400
-
+    # 2. Extract Data (Same as before)
     try:
         row_to_insert = [
             {
                 "event_ts": datetime.datetime.utcnow().isoformat(),
-                "device_id": request_data.get("device_id", "unknown"),
-                # We cast to int/float because URL params are always strings
-                "moisture_raw": int(request_data.get("raw", 0)),
-                "moisture_pct": int(request_data.get("pct", 0)),
-                "battery_volts": float(request_data.get("bat", 0.0)),
-                "meta_data": "via_get"
+                "device_id": request_args.get("device_id", "unknown"),
+                "moisture_raw": int(request_args.get("raw", 0)),
+                "moisture_pct": int(request_args.get("pct", 0)),
+                "battery_volts": float(request_args.get("bat", 0.0)),
+                "meta_data": "via_secure_get"
             }
         ]
         
